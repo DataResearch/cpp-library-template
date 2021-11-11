17822cbd4456e91b2900854ee2dc006777f27a27
Christian Schmidt <developgames@gmx.de>
2021-11-11 18:02:46 +0100

    laaa

--- src/include/project/library.h
+++ src/include/project/library.h
@@ -1,5 +1,19 @@
+/*!
+ * This is documentation.
+ */
 namespace company::project {
 
-int function(int, int);
+/*!
+ * A brief history of JavaDoc-style (C-style) comments.
+ *
+ * This is the typical JavaDoc-style C-style comment. It starts with two
+ * asterisks.
+ *
+ * \brief { add values }
+ *
+ * \param a The first sum value thing.
+ * \param b The second sum value thing.
+ */
+int function(int a, int b);
 
-}
+} // namespace company::project
